33cc3a40fbcb4995e0c81761332605824eb941a3
jemilbu <jemilbu@ksu.edu>
2021-09-24 12:29:23 -0500

    Lecture 14 Update

--- first_largest.py
+++ first_largest.py
@@ -26,8 +26,28 @@ def largest_el2(a, loc=False):
         return maxval, maxLoc
     return maxval
 
+def largest(a,findLoc=False):
+    """ Return the largest element of a sequence a. Error Checks
+    """
+    try: 
+        maxVal = a[0]
+        maxLoc = 0
+        for i in range(1,len(a)):
+            if a[i] > maxVal:
+                maxVal = a[i]
+                maxLoc = i
+
+        if findLoc == True:
+            return maxVal, maxLoc
+        return maxVal
+    except TypeError:
+        print("Can't Find Max or Location, Error in Types")
+        return -1,-1
+    except:
+        raise
+
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
-    r,l = largest_element(a,True)
-    print("Largest element is {:}, Location {:}".format(r,l))
+    a = [1,3,2]
+    #r, l = largest(a,True)
+    print("Largest element is {:}, Location {:}".format(*largest(a,True)))
